2efd24a07f21820005a799d26c178389c83cfa65
Audvy <audvy@audvy.me>
2024-12-09 23:13:50 -0500

    Added PacketReader and PacketBuilder to allow for sending of data between Client and Server

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static List<Client> _users;
+        static TcpListener _listener;
+        static void Main(string[] args)
+        {
+            _users = new List<Client>();
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7890);
+            _listener.Start();
+
+            while (true)
+            {
+                var client = new Client(_listener.AcceptTcpClient());
+                Console.WriteLine($"{client.UID} has connected on port 7890!");
+                _users.Add(client);
+
+                /*Broadcast connection to everyone on the server */
+            }
+        }
+    }
+}
